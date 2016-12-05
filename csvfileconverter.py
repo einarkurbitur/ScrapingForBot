a70eefa170c19ea32f50549116a3c4599b82e531
Dirk van Oosterbosch <developer@irlabs.nl>
2016-12-05 08:36:25 +0100

    Showing how to include the use of the TextFixer

--- csvfileconverter.py
+++ csvfileconverter.py
@@ -1,11 +1,12 @@
 import csv
+from TextFixer import filter_tweet
 
 outtext = ""
 
 with open('SpaceX_tweets.csv') as f:
      reader = csv.DictReader(f)
      for row in reader:
-         outtext += row['text'] + ".\n"
+         outtext += filter_tweet(row['text']) + ".\n"
 
 with open('spacex_out.txt', 'w') as f:
      f.write(outtext)
