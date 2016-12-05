d5600961016b38c05275be1a105ed3f4ad36ecc4
Dirk van Oosterbosch <developer@irlabs.nl>
2016-12-05 08:13:27 +0100

    Show how to save an output from python.
    (Warning, I did not run this to test it.)

--- csvfileconverter.py
+++ csvfileconverter.py
@@ -5,4 +5,7 @@ outtext = ""
 with open('SpaceX_tweets.csv') as f:
      reader = csv.DictReader(f)
      for row in reader:
-         print( row['text'] + ".")
+         outtext += row['text'] + ".\n"
+
+with open('spacex_out.txt', 'w') as f:
+     f.write(outtext)
